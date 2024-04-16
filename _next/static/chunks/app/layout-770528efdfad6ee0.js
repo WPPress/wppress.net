f56ace8b24aef3e66954325cdc0061ec2c559c2d
ssovit <ssovit@users.noreply.github.com>
2024-04-16 16:20:32 +0000

    deploy: b234719a5d0bd8bbe3150289d9d0c0f911e83a22

---
+++ _next/static/chunks/app/layout-770528efdfad6ee0.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{7375:function(e,n,t){Promise.resolve().then(t.t.bind(t,231,23)),Promise.resolve().then(t.t.bind(t,6704,23)),Promise.resolve().then(t.t.bind(t,2563,23))},2563:function(){}},function(e){e.O(0,[546,70,971,23,744],function(){return e(e.s=7375)}),_N_E=e.O()}]);
